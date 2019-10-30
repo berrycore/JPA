814f57bf1bdd92403ebc8e14893ab2d1bfb6bbd8
SanghoonJeong <berrycore@gmail.com>
2019-10-30 17:39:00 +0900

    How to mapping a EnumType

--- JPA/src/main/java/jpabook/start/Member.java
+++ JPA/src/main/java/jpabook/start/Member.java
@@ -1,44 +1,103 @@
 package jpabook.start;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name="MEMBER")
+@Table(name = "MEMBER", uniqueConstraints = {
+		@UniqueConstraint(
+				name="NAME_AGE_UNIQUE", 
+				columnNames = {"NAME", "AGE"}
+			)
+		}
+)
 public class Member {
 
 	@Id
-	@Column(name="ID")
+	@Column(name = "ID")
 	private String id;
-	
-	@Column(name="NAME")
+
+	@Column(name = "NAME", nullable = false, length = 10)
 	private String username;
-	
+
 	private Integer age;
-	
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	@Lob
+	private String description;
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public Integer getAge() {
 		return age;
 	}
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-	
-	@Override
-	public String toString() {
-		return "Member [id=" + id + ", username=" + username + ", age=" + age + "]";
+
+	public RoleType getRoleType() {
+		return roleType;
 	}
+
+	public void setRoleType(RoleType roleType) {
+		this.roleType = roleType;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 }
